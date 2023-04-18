279a208b133fef3704d9b4329be1f301d026f14d
Sungsik Nam <jmyl@me.com>
2023-04-18 16:46:55 +0200

    My key map with original home row mods

---
+++ keyboards/keycapsss/3w6_2040/keymaps/default/config.h
@@ -0,0 +1,31 @@
+
+// Set the mouse settings to a comfortable speed/accuracy trade-off,
+// assuming a screen refresh rate of 60 Htz or higher
+// The default is 50. This makes the mouse ~3 times faster and more accurate
+#define MOUSEKEY_INTERVAL 16
+// Step size
+#define MOUSEKEY_MOVE_DELTA 8
+// Maximum cursor speed at which acceleration stops
+#define MOUSEKEY_MAX_SPEED 3
+// The default is 20. Since we made the mouse about 3 times faster with the previous setting,
+// give it more time to accelerate to max speed to retain precise control over short distances.
+#define MOUSEKEY_TIME_TO_MAX 40
+// The default is 300. Let's try and make this as low as possible while keeping the cursor responsive
+#define MOUSEKEY_DELAY 10
+// It makes sense to use the same delay for the mouseweel
+#define MOUSEKEY_WHEEL_DELAY 100
+// The default is 100
+#define MOUSEKEY_WHEEL_INTERVAL 50
+// The default is 40
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 100
+
+//// Configure the global tapping term (default: 200ms)
+#define TAPPING_TERM 200
+
+// Prevent normal rollover on alphas from accidentally triggering mods.
+#define IGNORE_MOD_TAP_INTERRUPT
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+// #define QUICK_TAP_TERM 0
+// Time (in ms) before the one shot key is released
+#define ONESHOT_TIMEOUT 100
