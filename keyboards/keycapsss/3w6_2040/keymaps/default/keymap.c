e0241de39905bf3b191fc691a23b8aea29cdad02
Sungsik Nam <ssnam@vueron.org>
2023-12-10 23:33:07 +0100

    Map all homerow mod keys

--- keyboards/keycapsss/3w6_2040/keymaps/default/keymap.c
+++ keyboards/keycapsss/3w6_2040/keymaps/default/keymap.c
@@ -15,19 +15,20 @@ enum layers {
 // Left-hand home row mods
 #define HOME_A LCTL_T(KC_A)
 #define HOME_S LSFT_T(KC_S)
-#define HOME_D KC_D
-#define HOME_F KC_F
-#define HOME_G KC_G
+#define HOME_D LALT_T(KC_D)
+#define HOME_F LGUI_T(KC_F)
+#define HOME_G RALT_T(KC_G)
 #define HOME_V KC_V
+#define HOME_C KC_C
 #define HOME_Z LT(_MOV, KC_Z)
 
-#define HOME_SPACE LGUI_T(KC_SPACE)
+#define HOME_SPACE RALT_T(KC_SPACE)
 
 // Right-hand home row mods
-#define HOME_SC LCTL_T(KC_SCLN)
-#define HOME_L KC_L
-#define HOME_K KC_K
-#define HOME_J KC_J
+#define HOME_SC RCTL_T(KC_SCLN)
+#define HOME_L RSFT_T(KC_L)
+#define HOME_K LALT_T(KC_K)
+#define HOME_J RGUI_T(KC_J)
 #define HOME_H KC_H
 #define HOME_M KC_M
 
@@ -37,20 +38,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_ALPHA_QWERTY] = LAYOUT_split_3x5_3(
         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    /**/  KC_Y,    KC_U,    KC_I,     KC_O,    KC_P,
         HOME_A,  HOME_S,  HOME_D,  HOME_F,  HOME_G,  /**/  HOME_H,  HOME_J,  HOME_K,   HOME_L,  HOME_SC,
-        HOME_Z,  KC_X,    KC_C,    HOME_V,  KC_B,    /**/  KC_N,    HOME_M,  KC_COMM,  KC_DOT,  KC_SLSH,
+        HOME_Z,  KC_X,    HOME_C,  HOME_V,  KC_B,    /**/  KC_N,    HOME_M,  KC_COMM,  KC_DOT,  KC_SLSH,
 
         XXXXXXX,  MO(_NUM),  KC_LSFT,  /**/  MO(_SYM),  HOME_SPACE,  XXXXXXX
     ),
     [_NUM] = LAYOUT_split_3x5_3(
         KC_1,     KC_2,       KC_3,     KC_4,     KC_5,        /**/  KC_6,     KC_7,     KC_8,     KC_9,     KC_0,
-        KC_TAB,   S(KC_TAB),  KC_DEL,   KC_BSPC,  S(KC_F10),     /**/  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_ENTER,
+        KC_TAB,   S(KC_TAB),  KC_DEL,   KC_BSPC,  S(KC_F10),   /**/  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_ENTER,
         C(KC_Z),  C(KC_X),    C(KC_C),  C(KC_V),  S(C(KC_V)),  /**/  KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END,   XXXXXXX,
 
         _______,  _______,  _______,  /**/  _______,  KC_ESC,  XXXXXXX
     ),
     [_SYM] = LAYOUT_split_3x5_3(
-        KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,/**/  KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,
-        KC_GRV,   KC_DQUO,  KC_MINS,  KC_UNDS,  KC_LBRC,/**/  KC_RBRC,  KC_BSPC,   KC_PLUS,  KC_QUOT,  KC_COLN,
+        KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,/**/  KC_CIRC,  KC_AMPR, KC_ASTR,  KC_LPRN,  KC_RPRN,
+        KC_GRV,   KC_DQUO,  KC_MINS,  KC_UNDS,  KC_LBRC,/**/  KC_RBRC,  KC_BSPC, KC_PLUS,  KC_QUOT,  KC_COLN,
         KC_BSLS,  KC_PIPE,  KC_TILD,  KC_0,     KC_LCBR,/**/  KC_RCBR,  KC_EQL,  KC_LT,    KC_GT,    KC_QUES,
 
         _______,  _______,  _______,/**/  _______,  _______,  XXXXXXX
